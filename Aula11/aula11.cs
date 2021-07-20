d63606b11b1438f630a385e60d3678a5b9ef89cf
Marllon Campos <marlloncampos@hotmail.com>
2021-07-19 23:50:02 -0300

    feat: ended eleventh lesson, learned what is type cast and how to use it on convert types

---
+++ Aula11/aula11.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Aula11 {
+
+    static void Main(){
+        // float n1 = 10.5f;
+        // int n2 = (int)n1 Using type cast
+
+        int vInt =10;
+        short vShort = (short)vInt;
+
+        System.Console.WriteLine(vShort);
+
+    }
+}
