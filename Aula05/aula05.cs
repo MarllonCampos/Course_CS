82f361a14f9d86ccd1d9c055e1c21691cd420875
marlloncampos <marllondcsp@gmail.com>
2021-07-19 22:22:39 -0300

    feat: ended fifth lesson, learned how to increment, decrement, divide a number in short hand

---
+++ Aula05/aula05.cs
@@ -0,0 +1,21 @@
+using System; 
+
+class Aula05{
+  static void Main() {
+    int num = 10;
+
+    num = num + 1;
+    Console.WriteLine(num);
+
+    num += 1;
+    Console.WriteLine(num);
+
+    num++;
+    Console.WriteLine(num);
+
+
+
+
+
+  }
+}
