60cbc579ba881be0786e2e9667c284d049108e9f
marlloncampos <marllondcsp@gmail.com>
2021-07-19 22:20:48 -0300

    feat: ended third lesson, learned how to make simple operations, as multiplication, and how to print it on console

---
+++ Aula03/aula03.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Aula03
+{
+
+  static void Main()
+  {
+    int num1, num2, res;
+
+    num1 = 10;
+
+    num2 = 2;
+
+    res = num1 * num2;
+
+    Console.Write("A multiplicação de {0} * {1} é igual a {2}",num1,num2,res);
+  }
+}
