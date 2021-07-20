ee02c21085d11e69b379f30a67f15fae5ffc28dc
Marllon Campos <marlloncampos@hotmail.com>
2021-07-19 23:09:40 -0300

    feat: ended seventh lesson, learned how to create constant variables to never change while the code is running

---
+++ Aula07/aula07.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Aula07{
+
+    static void Main() {
+        const string canal="Curso de C#";
+        const double pi=3.1415;
+
+        Console.WriteLine("Canal {0}\nPi: {1}", canal,pi);
+
+    }
+
+}
