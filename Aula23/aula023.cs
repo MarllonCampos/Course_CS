1d49b8378c9ce953ce8ef99fc99239709498355e
Marllon Campos <marlloncampos@hotmail.com>
2021-07-29 23:51:05 -0300

    feat: learning a lot of array methods, now seeing binarysearch who search on array and it return the position of the passed number, if didn't find it will return -1

---
+++ Aula23/aula023.cs
@@ -0,0 +1,46 @@
+using System;
+
+class Aula23
+{
+
+  public static void Main()
+  {
+    int[] vetor1 = new int[5];
+    int[] vetor2 = new int[5];
+    int[] vetor3 = new int[5];
+    int[,] matriz = new int[2, 5] { { 11, 20, 00, 44, 55 }, { 66, 77, 88, 99, 00 } };
+
+    Random random = new Random();
+
+    for (int i = 0; i < vetor1.Length; i++)
+    {
+      vetor1[i] = random.Next(50);
+    }
+
+
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Elementos do vetor 1");
+    foreach (int n in vetor1)
+    {
+      Console.WriteLine(n);
+    }
+
+    // public static int BinarySearch(array,valor)
+    Console.WriteLine("BinarySearch");
+    int procurado = 33;
+    int pos = Array.BinarySearch(vetor1, procurado);
+    Console.WriteLine("Valor {0} está na posição {1}", procurado, pos);
+    Console.ForegroundColor = ConsoleColor.Gray;
+    Console.WriteLine("-----------------------------");
+
+
+
+
+
+
+
+
+
+
+  }
+}
